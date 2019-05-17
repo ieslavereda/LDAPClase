ab0ae70972883d308dbb7518c73e68b5abf7df09
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2019-05-17 14:56:22 +0200

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -24,8 +24,7 @@ public class Test {
 		ArrayList<Persona> listado = new ArrayList<Persona>();
 		
 		LDAP ldap = new LDAP();
-		
-		String NIA="10729387";
+	
 		String DNI="053052298S";
 		
 		//Listar miembros de un grupo
@@ -38,14 +37,6 @@ public class Test {
 		ldap.obtenerUsuarioLDAP(p);
 		System.out.println(p);
 		visualizarFoto(p.getFoto());
-		
-		
-		// Obtener un alumno por NIA
-		Alumno a = new Alumno();
-		a.establecerIdentificacion(NIA);
-		ldap.obtenerUsuarioLDAP(a);
-		System.out.println(a);
-		
 		
 		// Obtener una persona por uid
 		Persona persona = new Profesor();
@@ -66,22 +57,6 @@ public class Test {
 		conf.guardar();
 				
 	}
-	// metodo para visualizar foto
-	private static void visualizarFoto(byte[] foto){
-		TestFrame t = new TestFrame();
-		t.setVisible(true);
-		byte[] imgFoto = foto;
-
-		if (imgFoto != null) {
-
-			ImageIcon icono = new ImageIcon(imgFoto);
-			Image imageToResize = icono.getImage();
-			Image nuevaResized = imageToResize.getScaledInstance(t.label.getWidth(), t.label.getHeight(),
-					java.awt.Image.SCALE_SMOOTH);
-			icono = new ImageIcon(nuevaResized);
-
-			t.label.setIcon(icono);
-		}
-	}
+	
 
 }
